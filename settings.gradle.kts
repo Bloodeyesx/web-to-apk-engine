ab3e14b1a342bfbfaecde9bed32f0ef833c2388b
Bloodeyesx <94681981+Bloodeyesx@users.noreply.github.com>
2026-01-01 02:09:14 +0100

    Update project name and remove unused plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
         gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
         google()
         mavenCentral()
     }
 }
-plugins {
-    id("com.highcapable.sweetdependency") version "__SWEET_DEPENDENCY_VERSION__"
-    id("com.highcapable.sweetproperty") version "__SWEET_PROPERTY_VERSION__"
-}
-sweetProperty {
-    rootProject { all { isEnable = false } }
-}
-rootProject.name = "__PROJECT_NAME__"
+rootProject.name = "web-to-apk-engine"
 include(":app")
